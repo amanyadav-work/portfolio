299809071f3d06ead3d897e5a9dde69313027746
Aman Yadav <amansyadavwork@gmail.com>
2026-09-04 21:07:29 +0530

    resume fix, ui optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-
-
-
-
+import Resume from './components/Resume';
+import NotFound from './components/NotFound';
 
 function App() {
-
   return (
-    <>
-    
-<Home/>
-
-
-
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -82,7 +82,7 @@ const NavbarHome = ({ secRef, isMobile }) => {
 
   return (
     <>
-      <div className="cursor-circle"></div>
+      {!isMobile && <div className="cursor-circle"></div>}
       <div className="navhome">
         <h1 className='h4'>A.</h1>
         <div className='navbar-nav ul-nav'>
@@ -91,7 +91,7 @@ const NavbarHome = ({ secRef, isMobile }) => {
           <a className='nav-link rotate-link pl-2 pr-2' href="#section-3" >Experience</a>
           <a className='nav-link rotate-link pl-2 pr-2' href="#section-4" >Projects</a>
           <a className='nav-link rotate-link pl-2 pr-2' href="#section-5" >Contact</a>
-          <a className='nav-link rotate-link pl-2 pr-2' target='_blank' href="https://drive.google.com/file/d/10-wb8n5vyS-kvLKK0Q_qhyUrJEGxonYP/view" style={{ marginTop: "-13px" }}>CV<MdArrowOutward /></a>
+          <a className='nav-link rotate-link pl-2 pr-2' target='_blank' href="/resume" style={{ marginTop: "-13px" }}>CV<MdArrowOutward /></a>
         </div>
 
         <div className="col-4">
@@ -195,7 +195,7 @@ const NavbarHome = ({ secRef, isMobile }) => {
                     </li>
 
                   </ul>
-                  <a href="https://drive.google.com/file/d/10-wb8n5vyS-kvLKK0Q_qhyUrJEGxonYP/view" target="_blank" className='w-25 d-flex justify-content-center flex-column align-items-center edu-li'><MdDownload className='icon-blur h1' />CV</a>
+                  <a href="/resume" target="_blank" className='w-25 d-flex justify-content-center flex-column align-items-center edu-li'><MdDownload className='icon-blur h1' />CV</a>
                 </div>
               </div>
             </div>
