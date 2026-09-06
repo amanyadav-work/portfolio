ed0e55dc34575e8c6d3e3d6fc2133a67444c6fee
Aman Yadav <amansyadavwork@gmail.com>
2026-09-06 20:24:02 +0530

    Setup Cloudflare Workers deployment with OpenNext

---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UqfyXepJeiDwEaMvd+Ar/SF4NnPMfU7BxFGf5+8KsK4=\"\n}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
 };
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
