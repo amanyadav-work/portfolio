bd4f43ae8116290afb120ce145c75a0ec588af45
Aman Yadav <amansyadavwork@gmail.com>
2026-09-07 09:42:29 +0530

    analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: {
     default: "Aman Yadav - Software Development Engineer",
@@ -150,6 +150,7 @@ export default function RootLayout({ children }) {
           }}
         />
         {children}
+         <Analytics />
       </body>
     </html>
   );
